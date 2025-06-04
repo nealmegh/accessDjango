99da3e721b28240a891a4ab62d67739628ca4f80
Md Abrar Jahin Almazi Bipon <bipon.abrar@gmail.com>
2025-06-04 15:24:40 +0100

    changed setting for aws wsgi

--- accessTool/wsgi.py
+++ accessTool/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import os
+import sys
+import io
 from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accessTool.settings')
+#
+# application = get_wsgi_application()
+
+# 🩹 Patch stderr for Playwright + Apache/mod_wsgi compatibility
+if not hasattr(sys.stderr, "fileno"):
+    sys.stderr = io.StringIO()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accessTool.settings')
 
+# from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
